9b39c53e7ca8b02375b25fd383dfe765ba459f55
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-29 09:45:57 -0300

    add-corr-js-btn

--- todo.js
+++ todo.js
@@ -1,12 +1,13 @@
 function newTask() {
-    var task = document.getElementById("additem").value; //Add task
-    var addBtn = document.getElementsById("addBtn")[0];//Add button
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("- " + task));
-    ul.appendChild(li);
-    document.getElementById("additem").value = "";
-    document.getElementById("addBtn").value = "";
-    // li.onclick = removeItem;
-    console.log(additem);
+  document.getElementById ("list").innerHTML="addItem";
+    // var task = document.getElementById("addItem").value; //Add task
+    // var addBtn = document.getElementsById("addBtn")[0];//Add button
+    // var ul = document.getElementById("list");
+    // var li = document.createElement("li");
+    // li.appendChild(document.createTextNode("- " + task));
+    // ul.appendChild(li);
+    // document.getElementById("addItem").value = "";
+    // document.getElementById("addBtn").value = "";
+    // // li.onclick = removeItem;
+    // console.log(addItem);
   }
